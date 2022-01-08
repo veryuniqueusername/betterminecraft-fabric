8c81309b835fa654c15305e99cb2e2ae6d9554a2
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2022-01-08 15:13:39 +0100

    Update BetterMinecraft.java

--- src/main/java/io/github/veryuniqueusername/betterminecraft/BetterMinecraft.java
+++ src/main/java/io/github/veryuniqueusername/betterminecraft/BetterMinecraft.java
@@ -10,7 +10,7 @@ public class BetterMinecraft implements ModInitializer {
 	public static final String MOD_ID = "betterminecraft";
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-	
+
 	@Override
 	public void onInitialize() {
 		init();
